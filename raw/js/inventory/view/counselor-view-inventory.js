c670b6168ec8d764cdcfc4f434fb1031e1cd2c03
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-13 20:56:04 +0700

    Fix counselor view profile

--- raw/js/inventory/view/counselor-view-inventory.js
+++ raw/js/inventory/view/counselor-view-inventory.js
@@ -8,6 +8,11 @@
 
 (function ($)
 {
+    audiojs.events.ready(function ()
+    {
+        var aj = audiojs.createAll();
+    });
+
     $('table#inventory_tb').DataTable();
     $(function ()
     {
