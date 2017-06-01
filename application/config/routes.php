cdf029bc5f8cbff29360520707110fb447f27feb
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-02 04:28:08 +0700

    Update route

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'welcome';
 $route['inventory/edit/(:num)'] = 'inventory/edit/$1';
+$route['student/detail/(:num)'] = 'student/detail/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
